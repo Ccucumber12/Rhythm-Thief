9758d18f0362bcdaa97f00193a97e8dc2cba7264
Chi-Feng Tsai <ktpss95112@gmail.com>
2023-11-03 18:22:50 +0800

    temp commit for chiffoncake

---
+++ Assets/Scripts/Sheet.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sheet : MonoBehaviour {
+    public double PixelPerSecond;
+
+    void Start() {
+
+    }
+
+    void Update() {
+
+    }
+}
