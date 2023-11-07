f6519a50a7e43f59b820ca34cd1214ac3126f039
anj226 <andruw0205@gmail.com>
2023-11-07 19:22:32 +0800

    add player walking animation

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -35,6 +35,7 @@ public class Player : MonoBehaviour
     private float lastInputTime;
     private float lastFireTime;
     private Tween moveTween;
+    private Animator animator;
 
     private void Awake()
     {
@@ -44,6 +45,7 @@ public class Player : MonoBehaviour
             DestroyImmediate(gameObject);
 
         playerSprite = transform.Find(Sprite_Name).gameObject;
+        animator = playerSprite.GetComponent<Animator>();
     }
 
     private void Start()
@@ -88,21 +90,29 @@ public class Player : MonoBehaviour
             TryMove(Vector2.right);
     }
 
+    private void AnimationStop()
+    {
+        animator.SetBool("Walking", false);
+    }
+
     private void TryMove(Vector2 direction)
     {
         if (Time.time < lastInputTime + inputCoolDown)
             return;
         lastInputTime = Time.time;
+        animator.SetFloat("X", direction[0]);
+        animator.SetFloat("Y", direction[1]);
         if (rhythmManager.CheckMove() || true) // TODO
         {
             facingDirection = direction;
             Vector3 newPosition = transform.position + MathUtils.GetVector3FromVector2(direction);
             Collider2D cols = Physics2D.OverlapCircle(newPosition, 0.1f, wallLayer);
+            animator.SetBool("Walking", true);
             if (cols == null)
             {
                 // move success
                 moveTween?.Kill(complete: true);
-                moveTween = transform.DOMove(newPosition, 0.1f);
+                moveTween = transform.DOMove(newPosition, 0.1f).OnComplete(AnimationStop);
                 UpdateSpriteDirection();
             }
             else
@@ -138,9 +148,9 @@ public class Player : MonoBehaviour
 
     private void UpdateSpriteDirection()
     {
-        Vector3 scale = playerSprite.transform.localScale;
-        scale.x = Mathf.Abs(scale.x) * Mathf.Sign(facingDirection.x);
-        playerSprite.transform.localScale = scale;
+        // Vector3 scale = playerSprite.transform.localScale;
+        // scale.x = Mathf.Abs(scale.x) * Mathf.Sign(facingDirection.x);
+        // playerSprite.transform.localScale = scale;
     }
 
     [System.Serializable]
