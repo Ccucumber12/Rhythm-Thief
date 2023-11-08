58ba873ec062fb9e4368094defdba76aea3e7e1b
Chi-Feng Tsai <ktpss95112@gmail.com>
2023-11-08 18:00:24 +0800

    add new prefab for newer sheet

---
+++ Assets/Scripts/Sheet20.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sheet20 : MonoBehaviour {
+    [SerializeField] private GameObject PrefabMoveIcon;
+    [SerializeField] private GameObject PrefabDoorIcon;
+    [SerializeField] private GameObject PrefabLightIcon;
+    [SerializeField] private GameObject PrefabBellIcon;
+    [SerializeField] private GameObject PrefabDiscoIcon;
+
+
+
+    // Start is called before the first frame update
+    void Start() {
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+    }
+}
