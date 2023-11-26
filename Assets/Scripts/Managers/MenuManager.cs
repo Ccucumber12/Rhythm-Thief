d149fa55641ec089a4acee2eddc58daaaf789c2e
Chi-Feng Tsai <ktpss95112@gmail.com>
2023-11-27 00:49:48 +0800

    add menu scene

--- Assets/Scripts/Managers/MenuManager.cs
+++ Assets/Scripts/Managers/MenuManager.cs
@@ -1,9 +1,45 @@
+using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class MenuManager : MonoBehaviour
 {
-    public string stageSceneName;
+    [SerializeField] private List<Stage> initStages;
+    private List<Stage> stages;
+
+    private int _focusIndex = 0;
+    private int focusIndex {
+        get => _focusIndex;
+        set {
+            if (value < 0 || value >= stages.Count) return;
+            _focusIndex = value;
+            updateFocus();
+        }
+    }
+
+    void Start() {
+        foreach (Stage stage in initStages) {
+            stage.highlight.SetActive(false);
+        }
+        stages = initStages.FindAll(stage => stage.enabled);
+        focusIndex = 0;
+    }
+
+    private void updateFocus() {
+        if (stages.Count == 0) return;
+        for (int i = 0; i < stages.Count; ++i) {
+            stages[i].highlight.SetActive(i == focusIndex);
+        }
+    }
+
+    public void OnChangeFocusMinus(InputValue value) {
+        focusIndex -= 1;
+    }
+
+    public void OnChangeFocusPlus(InputValue value) {
+        focusIndex += 1;
+    }
 
     public void OnReturnToStart(InputValue value)
     {
@@ -12,6 +48,14 @@ public class MenuManager : MonoBehaviour
 
     public void OnSelectStage()
     {
-        GameManager.Instance.SelectStage(stageSceneName);
+        GameManager.Instance.SelectStage(stages[focusIndex].stageSceneName);
     }
+}
+
+
+[System.Serializable]
+class Stage {
+    public string stageSceneName;
+    public GameObject highlight;
+    public bool enabled = true;
 }
