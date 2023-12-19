7b674e909516391ac7fee2d1c7f1840fd0c6563d
Ccucumber12 <chris20021112@gmail.com>
2023-12-19 19:58:57 +0800

    update in game pause menu

--- Assets/Scripts/UI/PauseController.cs
+++ Assets/Scripts/UI/PauseController.cs
@@ -6,25 +6,13 @@ using UnityEngine.UI;
 
 public class PauseController : MonoBehaviour
 {
+    [SerializeField] Button firstSelectButton;
+
     private Canvas canvas;
     private InGameManager inGameManager;
-    private AudioManager audioManager;
     private PlayerInput input;
 
-    private int _focus;
-    public int focus {
-        get {return _focus;}
-        set {
-            if (value < 0 || value >= 3) return;
-            _focus = value;
-            updateFocus();
-        }
-    }
-
-    [SerializeField] private Button resumeButton;
-    [SerializeField] private Button restartButton;
-    [SerializeField] private Button returnButton;
-
+    private GameManager gameManager;
 
     private void Awake()
     {
@@ -39,12 +27,7 @@ public class PauseController : MonoBehaviour
         inGameManager.onGameResumed.AddListener(DisablePauseScreen);
         DisablePauseScreen();
 
-        audioManager = AudioManager.Instance;
-
-        // setup callback
-        resumeButton.onClick.AddListener(() => inGameManager.ResumeGame());
-        restartButton.onClick.AddListener(() => GameManager.Instance.UpdateGameState(GameState.InGame));
-        returnButton.onClick.AddListener(() => GameManager.Instance.UpdateGameState(GameState.StageSelect));
+        gameManager = GameManager.Instance;
     }
 
     private void OnDestroy()
@@ -53,61 +36,31 @@ public class PauseController : MonoBehaviour
         inGameManager.onGameResumed.RemoveListener(DisablePauseScreen);
     }
 
+    public void GameResume()
+    {
+        inGameManager.ResumeGame();
+    }
+
+    public void GameRestart()
+    {
+        gameManager.UpdateGameState(GameState.InGame);
+    }
+
+    public void GameReturn()
+    {
+        gameManager.UpdateGameState(GameState.StageSelect);
+    }
+
     public void EnablePauseScreen()
     {
         input.enabled = true;
         canvas.enabled = true;
-        focus = 0;
+        firstSelectButton.Select();
     }
 
     public void DisablePauseScreen()
     {
         input.enabled = false;
         canvas.enabled = false;
-    }
-
-    public void FocusIncr() {
-        if (focus == 2) audioManager.Play("SelectOut");
-        else audioManager.Play("Select");
-
-        focus += 1;
-    }
-
-    public void FocusDecr() {
-        if (focus == 0) audioManager.Play("SelectOut");
-        else audioManager.Play("Select");
-
-        focus -= 1;
-    }
-
-    void updateFocus() {
-        Button focusedButton = (focus == 0) ? resumeButton
-                             : (focus == 1) ? restartButton
-                             : (focus == 2) ? returnButton
-                             : null;
-        Button[] buttons = {resumeButton, restartButton, returnButton};
-        foreach (Button button in buttons) {
-            button.GetComponent<Image>().color = (button == focusedButton) ? Color.gray : Color.white;
-        }
-    }
-
-    public void OnChangeFocusMinus(InputValue value) {
-        FocusDecr();
-    }
-
-    public void OnChangeFocusPlus(InputValue value) {
-        FocusIncr();
-    }
-
-    public void OnSelectFocusedOption(InputValue value) {
-        Button[] buttons = {resumeButton, restartButton, returnButton};
-        buttons[focus].onClick.Invoke();
-
-        audioManager.Play("Click");
-    }
-
-    public void OnReturnToGame(InputValue value)
-    {
-        inGameManager.ResumeGame();
     }
 }
